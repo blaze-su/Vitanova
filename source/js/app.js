902c3f9e898793ac759e0be75d3b4f965c78bc90
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2017-12-19 16:16:12 +0200

    change home page

--- source/js/app.js
+++ source/js/app.js
@@ -18,4 +18,19 @@ var slider = () => {
   var slideW = parseInt(document.getElementsByClassName('swiper-slide')[0].style.width);
   _slider.style.width = (document.getElementsByClassName('swiper-slide').length + 1) * slideW + 'px';
   document.getElementsByClassName('swiper-container')[0].classList.remove("-noVisible");
+
+  var mySwiper = new Swiper('.swiper-specialists', {
+    slidesPerView: 4,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  var mySwiper = new Swiper('.swiper-reviews', {
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
 };
